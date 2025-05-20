b537bd768b743849121450bfcabde319c8c38e50
José Isaac <joseisaacnascimento@gmail.com>
2025-05-20 09:05:09 -0300

    feat: Criação da api estática e de rotas

--- src/livros.js
+++ src/livros.js
@@ -1,12 +1,14 @@
+// API de livros
 const livros = [
   {
-    id: "",
-    capaURL: "",
-    nome: "",
-    descricao: "",
-    dataPubli: "",
-    nomeAutor: "",
-    resumo: "",
+    id: 1,
+    capaURL: "https://exemplo.com/imagem.jpg",
+    nome: "O Poder do Hábito",
+    descricao: "Como hábitos moldam nosso comportamento.",
+    dataPubli: "2012-02-28",
+    nomeAutor: "Charles Duhigg",
+    resumo:
+      "Este livro mostra como hábitos funcionam no cérebro e como podemos mudá-los para melhorar nossa vida.",
   },
 ];
 
