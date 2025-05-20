418553ddfcc11afc2a7dd34de8187730e18cbabe
José Isaac <joseisaacnascimento@gmail.com>
2025-05-20 08:51:33 -0300

    Commit inicial

---
+++ src/app.js
@@ -0,0 +1,2 @@
+const express = require("express"); // Importação do express
+
---
+++ src/livros.js
@@ -0,0 +1,13 @@
+const livros = [
+  {
+    id: "",
+    capaURL: "",
+    nome: "",
+    descricao: "",
+    dataPubli: "",
+    nomeAutor: "",
+    resumo: "",
+  },
+];
+
+module.exports = livros; // Exportação do array de livros
